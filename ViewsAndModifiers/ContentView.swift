93a7ab5d654b4edb761ad5d070e6116d94025f86
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-28 17:58:36 -0600

    why modifier order matters

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .background(.red)
+//        
+        Button("Hello, world!") {
+            // do nothing
         }
-        .padding()
+        .frame(width: 200, height: 200)
+        .background(.red)
+        Text("Hello, world!")
+            .padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
     }
 }
 
