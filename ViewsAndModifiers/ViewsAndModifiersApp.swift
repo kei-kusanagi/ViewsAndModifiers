c6709953632b7636ece367a1143bcfe8936099f5
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-28 12:21:21 -0600

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Juan Carlos Robledo Morales on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
