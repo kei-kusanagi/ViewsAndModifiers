b7cd3ae732e87d9e9bf46a966e338655f6629fb9
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-28 19:45:32 -0600

    Custom modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,26 +7,49 @@
 
 import SwiftUI
 
-struct CapsuleText: View {
-    var text: String
+//struct Title: ViewModifier {
+//    func body(content: Content) -> some View {
+//        content
+//            .font(.largeTitle)
+//            .foregroundStyle(.white)
+//            .padding()
+//            .background(.blue)
+//            .clipShape(RoundedRectangle(cornerRadius: 10))
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello World")
+//            .modifier(Title())
+//    }
+//}
 
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .background(.indigo)
-            .clipShape(.capsule)
+struct AvatarStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.blue, lineWidth: 2))
+            .shadow(radius: 10)
     }
 }
-
+extension View {
+    func avatarStyle() -> some View {
+        modifier(AvatarStyle())
+    }
+}
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                .foregroundStyle(.white)
+        VStack {
+            Image("user_avatar")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .avatarStyle()
 
-            CapsuleText(text: "Second")
-                .foregroundStyle(.yellow)
+            Text("User Name")
+                .font(.headline)
+                .padding(.top, 10)
         }
     }
 }
